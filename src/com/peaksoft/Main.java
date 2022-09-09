adcf75eb153b9bdc5b2a65b46b7fdcbad01907e0
lesson1 <bayaliev07@gmail.com>
2022-09-09 20:16:36 +0300

    creat Autobaza

--- src/com/peaksoft/Main.java
+++ src/com/peaksoft/Main.java
@@ -18,41 +18,43 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Car[] car = {
-                Car.makeCar("M1", 9000, Model.BMW),
-                Car.makeCar("RS8", 6000, Model.AUDI),
-                Car.makeCar("Accord", 5000, Model.HONDA),
-                Car.makeCar("Abie", 8000, Model.TESLA)
-        };
-        String allCars = gson.toJson(car);
-        //  System .out.println(allCars);
-   //     writeFile(allCars);
-        System.out.println(readFile());
+//        Car[] car = {
+//                Car.makeCar("M1", 9000, Model.BMW),
+//                Car.makeCar("RS8", 6000, Model.AUDI),
+//                Car.makeCar("Accord", 5000, Model.HONDA),
+//                Car.makeCar("Abie", 8000, Model.TESLA)
+//        };
+//        String allCars = gson.toJson(car);
+//        //  System .out.println(allCars);
+//   //     writeFile(allCars);
+//        System.out.println(readFile());
+//    }
+//
+//    private static void writeFile(String object) {
+//        Path write = Paths.get(String.valueOf(WRITE_PATH));
+//
+//        try {
+//            Files.writeString(write, object, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+//        } catch (Exception e) {
+//            e.getMessage();
+//        }
+//    }
+//
+//    private static String readFile() {
+//        String json = "";
+//        try {
+//            FileReader reader = new FileReader(String.valueOf(WRITE_PATH));
+//            int a;
+//            while ((a = reader.read())   != -1) {
+//                json += (char)a;
+//            }
+//        } catch (Exception e) {
+//            e.getMessage();
+//        }
+//        return json;
+        Autobaza autobaza = new Autobaza(1,"Moscow");
+        System.out.println(autobaza);
     }
 
-    private static void writeFile(String object) {
-        Path write = Paths.get(String.valueOf(WRITE_PATH));
-
-        try {
-            Files.writeString(write, object, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
-        } catch (Exception e) {
-            e.getMessage();
-        }
-    }
-
-    private static String readFile() {
-        String json = "";
-        try {
-            FileReader reader = new FileReader(String.valueOf(WRITE_PATH));
-            int a;
-            while ((a = reader.read())   != -1) {
-                json += (char)a;
-            }
-        } catch (Exception e) {
-            e.getMessage();
-        }
-        return json;
-    }
-    Car[] cars = gson.fromJson(readFile(),Car[].class);
 
 }
